fdbb3aaa26e2c98593200738336e5996f458c4c9
Alexandru Serban <alexandru.serban@essensys.ro>
2018-02-13 14:04:10 +0200

    added bootstrap and connected redux store to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import QuestionList from './components/QuestionList';
 
 const AppDisplay = ({test}) => (
         <div>
             <h1>
                 Isomorphic react {test}
             </h1>
-        </div>
+            <div>
+                <QuestionList />
+            </div>
+        </div> 
     );
 
 const mapStateToProps = (state, ownProps) => {
---
+++ src/components/QuestionList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import TagList from './TagList';
+
+const QuestionListItem = ({title, tags}) => (
+    <div className="mb-3">
+        <h3>{title}</h3>
+        <div className="mb-2">
+            <TagList tags={tags}/>
+        </div>
+    </div>
+);
+
+const QuestionList = ({questions}) => (
+    <div>
+        { questions && questions.length ? 
+            <div>
+                { questions.map(q => <QuestionListItem key={q.question_id} {...q}/>)}
+            </div>
+            :
+            <div>
+                ...Loading questions
+            </div>
+        }
+    </div>
+);
+
+const mapStateToProps = ({questions}) => ({
+    questions
+});
+
+export default connect(mapStateToProps)(QuestionList);
